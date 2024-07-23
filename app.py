432af97ad253d2fb3e1b933d45e77e7015c4e83e
Mritunjay <pmritunjay635@gmail.com>
2024-07-23 20:09:07 +0530

    rizz

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-# import streamlit as st
 from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
 
-# Load the saved model
+
 model = joblib.load('multioutput_xgboost_model.pkl')
 
 @app.route('/')
 def home():
-    # return 'fuck jinja2'
-    return render_template('index.html')  # Create an HTML template for your website
+    
+    return render_template('index.html')  
 @app.route('/feasible',methods=['POST'])
 def feasible():
     try:
@@ -20,11 +19,10 @@ def feasible():
         # HCO3- 30-400
         # SO4-<500
         if(((predictions[:,2]<400) and (predictions[:,2]>30)) and (predictions[:,5]<500)):
-            return   '''since the CO3 content is {} and SO4 content is {} , hene
-                        water well construction is feasible,'''.format(predictions[:,2],predictions[:,5])
+            result = ('''since the CO3 content is {} and SO4 content is {} , henece water well construction is feasible,'''.format(predictions[:,2],predictions[:,5]))
         else:
-            return ('''the CO3 content is {} and SO4 content is {} , hence
-                     Not feasible to construct well '''.format(predictions[:,2],predictions[:,5]))
+            result =  ('''the CO3 content is {} and SO4 content is {} , hence Not feasible to construct well '''.format(predictions[:,2],predictions[:,5]))
+        return render_template('feasible.html', result=result)
     except Exception as e:
         return "error" 
     
@@ -32,31 +30,21 @@ def feasible():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get input data from the website's form or API request
+
         input1 = float(request.form['quantity'])
         input2 = float(request.form['quantity'])
-        # print(input1)
-        # print(input2)
-        # input_data=(input1,input2).toarray()
-        # input_data = request.form  # Adjust this based on your website's form structure
-        # input_data=[[input1,input2]]
 
-        # Preprocess the input data if needed
-
-        # Make predictions using the loaded model
         predictions = model.predict([[input1,input2]])
         if (((predictions[:,0]<8.5) and (predictions[:,0]>6.5)) and (predictions[:,14]<1300) and (predictions[:,1]<1000)):
-            return ("Your pH level of water is {}  , Electrical conductivity is {} while your TDS content is {} and therefore the water quality is good for drinking".format(predictions[:,0],predictions[:,1],predictions[:,14]))
+            result =  ("Your pH level of water is {}  , Electrical conductivity is {} while your TDS content is {} and therefore the water quality is good for drinking".format(predictions[:,0],predictions[:,1],predictions[:,14]))
         else:
-            return ("pH level is {}  , Electrical conductivity is {} while your TDS content is {}, and hence not safe for drinking".format(predictions[:,0],predictions[:,1],predictions[:,14]))
+            result =  ("pH level is {}  , Electrical conductivity is {} while your TDS content is {}, and hence not safe for drinking".format(predictions[:,0],predictions[:,1],predictions[:,14]))
 
-        # You can process the predictions further if necessary
-        # return predictions
-        # Return the predictions as JSON
-        # return jsonify({'your pH is:-': predictions.tolist()})
+        return render_template('predict.html', result=result)
+
     except Exception as e:
         return ('error')
-        # return jsonify({'error': str(e)})
+    
 
 if __name__ == '__main__':
     app.run(debug=True) 
